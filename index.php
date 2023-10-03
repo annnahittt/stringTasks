58ea6309a718f234d7c7f048b25662207b2bf0e0
annnahittt <khanahit2005@gmail.com>
2023-10-03 23:58:02 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+//$text = 'hello world ';
+//$letter ='hello';
+//$find = strpos($text, $letter);
+//if ($find === false){
+//    echo 'not found';
+//}else{
+//   echo $letter."<br>" . '  is in  '. $text;
+//}
+//
+//$link = 'wm-school.ru/php/index.php';
+//$filename = basename($link);
+//echo $filename;
+//
+//$string = '12345678';
+//$substring = substr($string, -3);
+//echo $substring;
+//
+//$text = 'hello  world';
+//$word = 'bye';
+//$replaced = str_replace( 'hello',$word, $text);
+//echo $replaced;
+//
+//$text = 'У лукоморья дуб зелёный; Златая цепь на дубе том; И днём и ночью кот учёный; Всё ходит по цепи кругом.';
+//$arr = explode(';', $text);
+//var_dump($arr);
+//
+//
+//$num = '0007947023.22';
+//$noZero = ltrim($num, "0");
+//echo $noZero;
+//
+//$text = 'https://www.wm-school.ru/7478639';
+//$position = strrpos($text, '/' );
+//echo substr($text,$position +1);
